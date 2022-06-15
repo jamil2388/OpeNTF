c687719b1d058d835f21cc7e57672c4c2ff92e17
karan96 <saxenak96@gmail.com>
2022-06-15 15:08:25 -0400

    (#157) Code Changes to include location

--- src/cmn/naive.py
+++ src/cmn/naive.py
@@ -1,21 +1,58 @@
 # from src.cmn.team import Team
 import os
-from src.cmn.patent import Patent
+from patent import Patent
 from src import param
-from src.cmn.team_test import Team
-
+#from src.cmn.team_test import Team
+from src import fnn_main
+# import src.main
+import numpy as np
+import json
+from json import JSONEncoder
+from sklearn.model_selection import KFold, train_test_split
 settings = param.settings
 
 datapath = '../../data/raw/uspt/toy.patent.tsv'
 filter = False
 
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+
+def create_evaluation_splits(n_sample, n_folds, train_ratio=0.85, output='./'):
+    train, test = train_test_split(np.arange(n_sample), train_size=train_ratio, random_state=0, shuffle=True)
+    splits = dict()
+    splits['test'] = test
+    splits['folds'] = dict()
+    skf = KFold(n_splits=n_folds, random_state=0, shuffle=True)
+    for k, (trainIdx, validIdx) in enumerate(skf.split(train)):
+        splits['folds'][k] = dict()
+        splits['folds'][k]['train'] = train[trainIdx]
+        splits['folds'][k]['valid'] = train[validIdx]
+
+    with open(f'{output}/splits.json', 'w') as f:
+        json.dump(splits, f, cls=NumpyArrayEncoder, indent=1)
+
+    return splits
+
+prep_output = f'../../data/preprocessed/uspt/{os.path.split(datapath)[-1]}'
+print(prep_output)
+output = f'../../output/'
+# exit()
 import os
 # print(os.listdir('../../data/preprocessed/'))
-print(os.listdir(f'../../data/preprocessed/uspt/{os.path.split(datapath)[-1]}'))
-print(f'../../data/preprocessed/uspt/{os.path.split(datapath)[-1]}')
+# print(os.listdir(f'../../data/preprocessed/uspt/{os.path.split(datapath)[-1]}'))
+# print(f'../../data/preprocessed/uspt/{os.path.split(datapath)[-1]}')
 # print(f'{os.path.split(datapath)[-1]}')
 vecs, indexes = Patent.generate_sparse_vectors(datapath, f'../../data/preprocessed/uspt/{os.path.split(datapath)[-1]}', filter, settings['data'])
-print('Reached')
+print(vecs)
+print('Reached. Starting fnn')
+splits = create_evaluation_splits(len(indexes['t2i']), 3, 0.85, output=prep_output)
+print(splits)
+fnn_main.main(splits, vecs, indexes, f'{output}{os.path.split(datapath)[-1]}test/fnn', settings['model']['baseline']['fnn'], settings['model']['cmd'])
 
 
 # T1 = Team()
--- src/cmn/team_test.py
+++ src/cmn/team_test.py
@@ -18,50 +18,82 @@ class Team(object):
         self.skills = skills
         self.country = country
 
-    def get_one_hot(self, l2i, c2i, locations):
-        # Generating one hot encoded vector for skills of team
-        # skill_vec_dim = len(s2i)
-        # X = np.zeros((1, skill_vec_dim))
-        # for field in self.skills: X[0, s2i[field]] = 1
-        print("In 1 hot encode")
-        print('Locations', locations)
-        #Generating one hot encoded vector for locations of team s2i =
-        loc_vec_dim = len(l2i)
-        X = np.zeros((1, loc_vec_dim))                                  #1,23: l2m
-        print("This is X", X)
-        for loc in locations:
-            print('In For')
-            print(l2i[loc])
-            #X[0, l2i[loc]] = 1
-        print('Modified X', X)
-        exit()
-        #print(self.country)
-        #print()
-        # for loc in self.country: print(loc)
-        #exit()
-
+    @staticmethod
+    def get_one_hot(c2i, teams):
         # Generating one hot encoded vector for members of team
-        candidate_vec_dim = len(c2i)                                    #1,24: m2l
-        y = np.zeros((1, candidate_vec_dim))
-        print('This is y', y)
-        #exit()
-        idnames = [f'{m.id}_{m.name}' for m in self.members]
-        print('This is len of idnames', len(idnames))
-        for idname in idnames:
-            print(idname)
-            if idname in c2i:
-                print('True')
-                break
-            y[0, c2i[idname]] = 1
-        print('Modified y',y)
-        exit()
-        id = np.zeros((1,1))
-        id[0,0] = self.id
-        X=X[:1]
-        y=y[:1]
-        print(np.hstack([id, X, y]))
-        exit()
-        return np.hstack([id, X, y])
+        candidate_vec_dim = len(c2i)
+
+        # y = np.zeros((len(teams), candidate_vec_dim))
+        y = np.ndarray(shape=(len(teams), candidate_vec_dim), dtype=int)
+        y.fill(0)
+        print('c2i is: ', c2i)
+        for ix, team in enumerate(teams.values()):
+            # print(dir(team.members))
+            for candidate in team.members:
+                idnames = [f'{candidate.id}_{candidate.name}']
+                for name in idnames:
+                    if name in c2i.keys():
+                        y[ix, c2i[name]] = 1
+            #             print(y)
+            #             exit()
+            # print('Next Team')
+        print('y for members is: ', y)
+        # candidate_vec_dim = len(c2i)
+        # y = np.zeros((1, candidate_vec_dim))
+        # idnames = [f'{m.id}_{m.name}' for m in self.members]
+        # for idname in idnames:
+        #     y[0, c2i[idname]] = 1
+        # id = np.zeros((1,1))
+        # id[0,0] = self.id
+        # id = np.zeros((1,1))
+        # id[0,0] = self.id
+        return y
+        # return np.hstack([id, x, y])
+
+    # def get_one_hot(self, l2i, c2i, locations):
+    #     # Generating one hot encoded vector for skills of team
+    #     # skill_vec_dim = len(s2i)
+    #     # X = np.zeros((1, skill_vec_dim))
+    #     # for field in self.skills: X[0, s2i[field]] = 1
+    #     print("In 1 hot encode")
+    #     print('Locations', locations)
+    #     #Generating one hot encoded vector for locations of team s2i =
+    #     loc_vec_dim = len(l2i)
+    #     X = np.zeros((1, loc_vec_dim))                                  #1,23: l2m
+    #     print("This is X", X)
+    #     for loc in locations:
+    #         print('In For')
+    #         print(l2i[loc])
+    #         #X[0, l2i[loc]] = 1
+    #     print('Modified X', X)
+    #     exit()
+    #     #print(self.country)
+    #     #print()
+    #     # for loc in self.country: print(loc)
+    #     #exit()
+    #
+    #     # Generating one hot encoded vector for members of team
+    #     candidate_vec_dim = len(c2i)                                    #1,24: m2l
+    #     y = np.zeros((1, candidate_vec_dim))
+    #     print('This is y', y)
+    #     #exit()
+    #     idnames = [f'{m.id}_{m.name}' for m in self.members]
+    #     print('This is len of idnames', len(idnames))
+    #     for idname in idnames:
+    #         print(idname)
+    #         if idname in c2i:
+    #             print('True')
+    #             break
+    #         y[0, c2i[idname]] = 1
+    #     print('Modified y',y)
+    #     exit()
+    #     id = np.zeros((1,1))
+    #     id[0,0] = self.id
+    #     X=X[:1]
+    #     y=y[:1]
+    #     print(np.hstack([id, X, y]))
+    #     exit()
+    #     return np.hstack([id, X, y])
 
     @staticmethod
     def build_index_candidates(teams):
@@ -97,38 +129,26 @@ class Team(object):
 
     @staticmethod
     def build_index_country(teams):
-        l2i = {}; m2l = {}; l2m = {}; i2l = {}
-        print('Entered Location Fn')
+        l2i = {}; i2l = {}
+        ix = 0
         for idx, t in enumerate(teams):
-            # print('t', dir(t))
-            # print('idx', idx)
-            for (candidate, loc) in zip(t.members, t.members_details):
-                idname = f'{candidate.id}_{candidate.name}'
-                if idname not in m2l:
-                    m2l[idname] = loc
-                if loc not in l2m:
-                    l2m[loc] = [idname]
-                else:
-                    l2m[loc] = l2m[loc].append(idname)
-                #print(idname)
-                #print(loc)
-            #print('members', t.members, 'member details', t.members_details)
-        print("This is l2m", len(l2m))
-        print("This is m2l", len(m2l))
-        #exit()
-            # for loc in t.members_details:
-            #     if loc not in l2i:
-            #         l2i[loc] = idx
-            #         i2l[idx] = loc
-            #         idx += 1
-        # print('i2l', i2l)
-        # print('l2i', l2i)
-        # exit()
-        locs = list(l2m.keys())
-        #with open(f'../../data/preprocessed/uspt/toy.patent.tsv/{locations.pkl}', 'wb') as outfile:
-        #    pickle.dump(locs, outfile)
-
-        return l2m, m2l, locs
+            for loc in t.members_details:
+                # print(l2i[loc])
+                if loc not in l2i.keys():
+                    l2i[loc] = ix
+                    ix += 1
+        col = list(l2i.values())[-1]
+        row = len(teams)
+        x = np.ndarray(shape=(row, col+1), dtype=int)
+        x.fill(0)
+        for rx, t in enumerate(teams):
+            print(t.members_details)
+            for one in t.members_details:
+                if one in l2i.keys():
+                    x[rx, l2i[one]] = 1
+        i2l = dict((v, k) for k, v in l2i.items())
+        print('x for location is: ', x)
+        return i2l, l2i, x
 
     # @staticmethod
     # def read_data(teams, output, filter, settings):
@@ -170,18 +190,22 @@ class Team(object):
         # build indexes
         indexes = {}
         print('Here in Teams read data')
+        # for val in teams.values():
+        #     print(val.members_details)
+        #
+        # exit()
         indexes['i2c'], indexes['c2i'] = Team.build_index_candidates(teams.values())
         indexes['i2s'], indexes['s2i'] = Team.build_index_skills(teams.values())
         indexes['i2t'], indexes['t2i'] = Team.build_index_teams(teams.values())
-        indexes['i2l'], indexes['l2i'], indexes['location_file'] = Team.build_index_country(teams.values())
-        print('locations in read_data', indexes['location_file'])
+        indexes['i2l'], indexes['l2i'], indexes['x'] = Team.build_index_country(teams.values())
+        # print('locations in read_data', indexes['location_file'])
         st = time()
 
         try: os.makedirs(output)
         except FileExistsError as ex: pass
 
-        with open(f'{output}/teams_v2.pkl', "wb") as outfile: pickle.dump(teams, outfile)
-        with open(f'{output}/indexes_v2.pkl', "wb") as outfile: pickle.dump(indexes, outfile)
+        with open(f'{output}/teams_v5.pkl', "wb") as outfile: pickle.dump(teams, outfile)
+        with open(f'{output}/indexes_v5.pkl', "wb") as outfile: pickle.dump(indexes, outfile)
         print(f"It took {time() - st} seconds to pickle the data into {output}")
         return indexes, teams
 
@@ -255,10 +279,56 @@ class Team(object):
     #     if j > -1: data[-(j+1):] = data_[0:j+1]
     #     return data
 
+    @staticmethod
+    def create_sparse_matrix(teams, x, y):
+        hstack = []
+        for ix, team in enumerate(teams.keys()):
+            X = x[ix]
+            id = np.zeros((1, 1))
+            id[0, 0] = team
+
+            X1 = np.zeros((1, X.shape[0]))
+            X1[0, :] = x[ix]
+
+            Y = y[ix]
+            Y1 = np.zeros((1, Y.shape[0]))
+            Y1[0, :] = y[ix]
+            hs = np.hstack([id, X1, Y1])
+            hstack.append(hs)
+        return hstack
+
+    @staticmethod
+    def bucketing_location(bucket_size, l2i, c2i, hstack):
+        location_vec_dim = len(l2i)
+        candidate_vec_dim = len(c2i)
+
+        data = lil_matrix((len(hstack), 1 + location_vec_dim + candidate_vec_dim))  #
+        data_ = np.zeros((bucket_size, 1 + location_vec_dim + candidate_vec_dim))
+        j = -1
+        st = time()
+        for i, team in enumerate(hstack):
+            try:
+                j += 1
+                data_[j] = team
+            except IndexError as ex:
+                s = int(((i / bucket_size) - 1) * bucket_size)
+                e = int(s + bucket_size)
+                data[s: e] = data_
+                j = 0
+                data_[j] = team
+            except Exception as ex:
+                raise ex
+
+            # if (i % bucket_size == 0): print(f'Loading {i}/{len(teams)} instances by {multiprocessing.current_process()}! {time() - st}')
+
+        if j > -1: data[-(j + 1):] = data_[0:j + 1]
+        return data
+
+
     @classmethod
     def generate_sparse_vectors(cls, datapath, output, filter, settings):
         output += f'.filtered.mt{settings["filter"]["min_nteam"]}.ts{settings["filter"]["min_team_size"]}' if filter else ""
-        pkl = f'{output}/teamsvecs_v2.pkl'
+        pkl = f'{output}/teamsvecs_v5.pkl'
         try:
             st = time()
             print(f"Loading sparse matrices from {pkl} ...")
@@ -267,19 +337,7 @@ class Team(object):
             indexes, _ = cls.read_data(datapath, output, index=False, filter=filter, settings=settings)
             print(f"It took {time() - st} seconds to load the sparse matrices.")
             return vecs, indexes
-            # st = time()
-            # # print(output)
-            # print(f"Loading sparse matrices from {pkl} ...")
-            # # print(pkl)
-            # with open(pkl, 'rb') as infile: vecs = pickle.load(infile)
-            # # print(datapath)
-            # with open(f'../../data/preprocessed/uspt/toy.patent.tsv/teams_v1.pkl', 'rb') as infile:
-            #     teams = pickle.load(infile)
-            #     print('Success')
-            #     exit()
 
-            #with open(f'{output}/teams.pkl', 'rb') as infile: teams = pickle.load(infile)
-            print('Hello')
             # exit()
             indexes, teams = cls.read_data(datapath, output, index=True, filter=filter, settings=settings)
             print(f"It took {time() - st} seconds to load the sparse matrices.")
@@ -295,55 +353,39 @@ class Team(object):
             #     func = partial(Team.bucketing, settings['bucket_size'], indexes['s2i'], indexes['c2i'])
             #     data = p.map(func, subteams)
             # serial
-            print('This is right before bucketing', indexes['location_file'])
-            print('horizontal length of vector: ', len(list(indexes['l2i'].keys())))
-            print('vertical length of vector: ', len(teams))
-            col = len(list(indexes['l2i'].keys()))
-            row = len(teams)
+            # print('Finished Creating data')
+            c2i = indexes['c2i']
 
-            x = np.ndarray(shape=(row, col), dtype=int)
-            x.fill(0)
+            x = indexes['x']
             # print(x)
-            loc_key = dict()
-            print(indexes['i2l'])
-            exit()
-            for ix, values in enumerate(teams):
-                # print(dir(teams[values]))
-                # break
-                # print(indexes['i2l'])
-                # break
-                # print('values for locs', teams[values].members_details)
-                for ixl, val in enumerate(list(indexes['i2l'].keys())):
-                    if val in teams[values].members_details:
-                        # print('team id', values)
-                        # print('associated locations', val)
-                        # print('Yes')
-                        # print('In If', val)
-                        if val not in list(loc_key.keys()):
-                            loc_key[val] = ixl
-                            x[ix, ixl] = 1
+            # team.get_one_hot(c2i, teams)
+            y = Team.get_one_hot(c2i, teams)
+            # print(teams.keys())
+            hstack = Team.create_sparse_matrix(teams, x, y)
 
-                        else:
-                            x[ix, loc_key[val]] = 1
-                    else:
-                        print('in else,', val)
-                        # print(x)
-                        # break
-                    # elif np.nan in val:
-                    #     # print('Found nan', val)
-                    #     x[ix, ixl] = 1
-
-                # print(ix)
-                # break
-            # print(loc_key)
-            print(x)
-            exit()
-            data = Team.bucketing(1000, indexes['l2i'], indexes['i2l'], indexes['location_file'], teams.values())
+            data = Team.bucketing_location(500, indexes['l2i'], c2i, hstack)
+            location_vec_dim = len(indexes['l2i'])
+            candidate_vec_dim = len(c2i)
+            # exit()
+            # print('This is right before bucketing')
+            # for team in teams.values():
+            #     # print(dir(team.members))
+            #     for candidate in team.members:
+            #         print(candidate.id)
+            #     print('Next Team')
+            #     # for t in team:
+            #     #     for candidate in team.members:
+            #     #         print(candidate)
+            #     #     break
+            # # for ix, team in teams.values():
+            # #     team.id)
+            # #     exit()
+            # exit()
+            # data = Team.bucketing(1000, indexes['l2i'], indexes['i2l'], indexes['location_file'], teams.values())
             data = scipy.sparse.vstack(data, 'lil')#{'bsr', 'coo', 'csc', 'csr', 'dia', 'dok', 'lil'}, By default an appropriate sparse matrix format is returned!!
             # vecs = {'id': data[:, 0], 'skill': data[:, 1:len(indexes['s2i']) + 1], 'member':data[:, - len(indexes['c2i']):]}
-            vecs = {'id': data[:, 0], 'location': data[:, 1:len(indexes['l2i']) + 1], 'member': data[:, - len(indexes['c2i']):]}
-            print(vecs)
-            exit()
+            vecs = {'id': data[:, 0], 'location': data[:, 1:location_vec_dim + 1], 'member': data[:, - candidate_vec_dim:]}
+
 
             with open(pkl, 'wb') as outfile: pickle.dump(vecs, outfile)
             print(f"It took {time() - st} seconds to generate and store the sparse matrices of size {data.shape} at {pkl}")
