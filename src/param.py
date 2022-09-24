ae0df3a11148b0b9353084a4e1cb8dde28ac42bb
hosseinfani <hosseinfani@gmail.com>
2022-09-24 02:14:56 -0400

    resolving conflict

--- src/param.py
+++ src/param.py
@@ -11,16 +11,28 @@ np.random.seed(0)
 settings = {
     'model':{
         'baseline': {
+            'random': {
+                'b': 4096
+            },
             'fnn':{
                 'l': [100],  # list of number of nodes in each layer
                 'lr': 0.1,  # learning rate
                 'b': 4096,  # batch size
-                'e': 100,  # epoch
+                'e': 20,  # epoch
                 'nns': None,  # number of negative samples
                 'ns': None,  # 'uniform', 'unigram', 'unigram_b'
             },
-            'bnn':{ # sample_elbo for bnn
-                's': 1  # number of sampling for bayesian nn
+            'bnn':{
+                'l': [100],  # list of number of nodes in each layer
+                'lr': 0.1,  # learning rate
+                'b': 4096,  # batch size
+                'e': 20,  # epoch
+                'nns': None,  # number of negative samples
+                'ns': None,  # 'uniform', 'unigram', 'unigram_b'
+                's': 1  # # sample_elbo for bnn
+            },
+            'nmt': {
+                'base_config': './mdl/nmt_config.yaml'
             },
             'emb':{
                 'd': 100,# embedding dimension
@@ -29,9 +41,10 @@ settings = {
                 'w': 1 #cooccurrence window
             }
         },
-        'cmd': ['train', 'test', 'eval', 'plot'],  # 'train', 'test', 'eval'
-        'nfolds': 3,
-        'train_test_split': 0.85
+        'cmd': ['train', 'test', 'eval', 'plot', 'agg'],  # 'train', 'test', 'eval', 'plot', 'agg'
+        'nfolds': 2,
+        'train_test_split': 0.85,
+        'step_ahead': 1,#for now, it means that whatever are in the last [step_ahead] time interval will be the test set!
     },
     'data':{
         'domain': {
@@ -39,10 +52,12 @@ settings = {
             'uspt':{},
             'imdb':{},
         },
+        'location_type': 'country', #should be one of 'city', 'state', 'country' and represents the location of members in teams (not the location of teams)
         'filter': {
-            'min_nteam': 5,
+            'min_nteam': 75,
             'min_team_size': 3,
         },
+        'parallel': 1,
         'ncore': 0,# <= 0 for all
         'bucket_size': 500
     },
