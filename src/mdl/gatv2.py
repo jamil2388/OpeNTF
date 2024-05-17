13a4738a95e8918570cfc2c0b39d2bc0811cb4f4
jamil2388 <ahmed491@uwindsor.ca>
2024-05-17 10:13:57 -0400

    changed to heads = 2

--- src/mdl/gatv2.py
+++ src/mdl/gatv2.py
@@ -6,7 +6,7 @@ from torch_geometric.nn import GATv2Conv, to_hetero
 from torch_geometric.data import Data,HeteroData
 
 class GATv2(torch.nn.Module):
-  def __init__(self, hidden_channels, heads=4, add_self_loops = False):
+  def __init__(self, hidden_channels, heads=2, add_self_loops = False):
     super().__init__()
     self.conv1 = GATv2Conv((-1, -1), hidden_channels, add_self_loops=add_self_loops, heads = heads)
     self.conv2 = GATv2Conv((-1, -1), hidden_channels, add_self_loops=add_self_loops, heads = heads)
